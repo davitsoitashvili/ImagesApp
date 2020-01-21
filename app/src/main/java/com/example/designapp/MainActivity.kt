cfa7abcbd29ea08e6ca10a72317266ca12e8a965
unknown <davit.soitashvili.1@btu.edu.ge>
2020-01-21 06:05:20 -0800

    update

--- app/src/main/java/com/example/designapp/MainActivity.kt
+++ app/src/main/java/com/example/designapp/MainActivity.kt
@@ -7,7 +7,6 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -16,46 +15,48 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.ImageView
 import java.io.IOException
 
-
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
     val imageArray = mutableListOf<Bitmap>()
-    var adapter : RecyclerAdapter = RecyclerAdapter(imageArray)
+    var adapter: RecyclerAdapter = RecyclerAdapter(imageArray)
 
     @RequiresApi(VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView : RecyclerView = recycler_view
+        val recyclerView: RecyclerView = recycler_view
         recyclerView.setLayoutManager(GridLayoutManager(this, 3))
         recyclerView.adapter = adapter
 
-        checkPermissions()
+        checkPermissions(take_image_id)
+        checkPermissions(upload_image_id)
     }
 
-    private fun checkPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
-                || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+    private fun checkPermissions(btn: ImageView) {
+        btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+                    || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+                ) {
 
-                val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                requestPermissions(permissions, PERMISION_CODE)
+                    val permissions = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    requestPermissions(permissions, PERMISION_CODE)
+                } else {
+                    openCamera()
+                    pickFromGallery()
+                }
             } else {
                 openCamera()
                 pickFromGallery()
             }
         }
-        else {
-            openCamera()
-            pickFromGallery()
-        }
     }
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -64,32 +65,32 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISION_CODE) {
-            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 openCamera()
                 pickFromGallery()
 
             } else {
-                Toast.makeText(this,"Permissions denied!",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Permissions denied!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     private fun openCamera() {
-        take_image_id.setOnClickListener {
-            val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(camera, REQUEST_CODE)
-        }
+
+        val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(camera, REQUEST_CODE)
 
     }
 
     private fun pickFromGallery() {
-        upload_image_id.setOnClickListener {
-            val gallery = Intent()
-            gallery.setType("image/*")
-            gallery.setAction(Intent.ACTION_GET_CONTENT)
 
-            startActivityForResult(Intent.createChooser(gallery, "Choose image"), GALLERY_REQUEST_CODE)
-        }
+        val gallery = Intent()
+        gallery.setType("image/*")
+        gallery.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(
+            Intent.createChooser(gallery, "Choose image"),
+            GALLERY_REQUEST_CODE
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -102,13 +103,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val  imageUris  = data?.data
+            val imageUris = data?.data
             try {
-                val photoGallery = MediaStore.Images.Media.getBitmap(contentResolver,imageUris) as Bitmap
+                val photoGallery =
+                    MediaStore.Images.Media.getBitmap(contentResolver, imageUris) as Bitmap
                 imageArray.add(photoGallery)
                 adapter.notifyDataSetChanged()
 
-            } catch (e : IOException) {
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
@@ -119,5 +121,4 @@ class MainActivity : AppCompatActivity() {
         val REQUEST_CODE = 100
         val GALLERY_REQUEST_CODE = 300
     }
-
 }
